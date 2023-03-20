63400959abfab243e2e1a74fe365953c8210e2d4
LE VY TU <tule0249@gmail.com>
2023-03-21 01:44:29 +0700

    rapphim

---
+++ Cinema/Cinema/Models/Dangky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    public class Dangky
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Ngaysinh { get; set; }
+        public string Email { get; set; }
+
+    }
+}
